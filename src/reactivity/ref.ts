978d71ec74e59315fb80691a9f4b16e901f47e34
muyeyong <xuyongshuaige@gmail.com>
2022-11-28 23:01:37 +0800

    feat: ref

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,20 @@
  * @FilePath: \mini-vue-myself\src\reactivity\ref.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-export const ref = (value) => {
 
+// ref 可以传入单个值
+class RefImpl {
+    private _value 
+    constructor(value) {
+        this._value = value
+    }
+    get value() {
+        return this._value
+    }
+    set value(val) {
+        this._value = val
+    }
+}
+export const ref = (value) => {
+    return new RefImpl(value)
 }
--- src/reactivity/test/ref.spec.ts
+++ src/reactivity/test/ref.spec.ts
@@ -3,5 +3,6 @@ import { ref } from "../ref"
 describe('ref', () => {
     it('happy path', () => {
         const count = ref(0)
+        expect(count.value).toBe(0)
     })
 })
